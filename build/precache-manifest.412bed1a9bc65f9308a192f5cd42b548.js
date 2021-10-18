3b9190501f806b907b858f89514bc4477b0c6645
Mateo Marin <mateomarin977@gmail.com>
2021-10-18 12:57:39 +0200

    make it better

---
+++ build/precache-manifest.412bed1a9bc65f9308a192f5cd42b548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a8c470396ce45804acf",
+    "url": "/static/css/main.fc1b587c.chunk.css"
+  },
+  {
+    "revision": "2a8c470396ce45804acf",
+    "url": "/static/js/main.6d8980cc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a15fafa6d6ab32cfd056",
+    "url": "/static/css/2.10b11d6b.chunk.css"
+  },
+  {
+    "revision": "a15fafa6d6ab32cfd056",
+    "url": "/static/js/2.8759e78f.chunk.js"
+  },
+  {
+    "revision": "af9aa98e0c6848574be939c5bf7cacfe",
+    "url": "/index.html"
+  }
+];
